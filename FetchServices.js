135a15f6cade451b55a95898de19a5ded5a4e54c
Shivam Chaturvedi <89623079+shiva3636@users.noreply.github.com>
2023-07-05 16:35:15 +0530

    Update FetchServices.js

--- FetchServices.js
+++ FetchServices.js
@@ -1,18 +1,97 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+const BaseURL = process.env.REACT_APP_BASEURL;
+const Token = "My Token";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
-);
+// POST Method
+const postService = async (url, body) => {
+  const response = await fetch(`${BaseURL}/${url}`, {
+    method: "POST",
+    headers: {
+      Authorization: Token,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  try {
+    const result1 = await response.json();
+    return result1;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// PUT Method
+const putService = async (url, body) => {
+  const response = await fetch(`${BaseURL}/${url}`, {
+    method: "PUT",
+    headers: {
+      Authorization: Token,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  try {
+    const result1 = await response.json();
+    return result1;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// GET Method
+const getService = async (path) => {
+  const response = await fetch(`${BaseURL}/${path}`, {
+    method: "GET",
+    headers: {
+      Authorization: Token,
+      "Content-Type": "application/json",
+    },
+  });
+  try {
+    const result2 = await response.json();
+    return result2;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//pass the FormData object using axios
+const postFormData = async (url, formData) => {
+  const response = await fetch(`${BaseURL}/${url}`, {
+    method: "POST",
+    headers: {
+      Authorization: Token,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: formData,
+  });
+  try {
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Pass the FormData object using axios
+const axiosPostData = async (url, formData) => {
+  return axios
+    .post(`${BaseURL}/${url}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: Token,
+      },
+    })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      if (err.response.status === 401) {
+      } else if (err.response.status !== 200) {
+      }
+    });
+};
+
+export { postServices, getServices, putServices, postFormData, axiosPostData};
